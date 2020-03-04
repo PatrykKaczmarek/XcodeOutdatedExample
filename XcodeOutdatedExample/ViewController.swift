3bc8b64baff32a12c31445f5575f573719e9714e
Patryk Kaczmarek <patryk.kaczmarek@netguru.pl>
2020-03-04 15:05:08 +0100

    Initial Commit

---
+++ XcodeOutdatedExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XcodeOutdatedExample
+//
+//  Created by Patryk Kaczmarek on 04/03/2020.
+//  Copyright © 2020 Netguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
